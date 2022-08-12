97f11acfabff89057e78e8bd956f0da3b8013481
bbelovic <bbelovic@gmail.com>
2022-08-12 21:52:38 +0200

    Example of stripIndent

--- src/test/java/com/oracle/lal/modernjava/textblocks/ExampleTest.java
+++ src/test/java/com/oracle/lal/modernjava/textblocks/ExampleTest.java
@@ -2,6 +2,12 @@ package com.oracle.lal.modernjava.textblocks;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ExampleTest {
 
     @Test
@@ -22,5 +28,19 @@ public class ExampleTest {
         System.out.println(html);
         System.out.println("========");
         System.out.println(html2.stripIndent());
+    }
+
+    @Test
+    public void indentation2() throws Exception {
+        var resource = getClass().getClassLoader().getResource("stripIndent.txt");
+        if (resource != null) {
+            var allLines = Files.readAllLines(Path.of(resource.toURI()));
+            var content = String.join("\n", allLines);
+            System.out.println(content);
+            System.out.println("========");
+            System.out.println(content.stripIndent());
+        } else {
+            System.out.println("Resource not found..");
+        }
     }
 }
