9aa3b0ced8a4e5f6cd0a08d714c930822ad54287
bbelovic <bbelovic@gmail.com>
2022-08-20 15:43:48 +0200

    Finished text block example

--- src/test/java/com/oracle/lal/modernjava/textblocks/ExampleTest.java
+++ src/test/java/com/oracle/lal/modernjava/textblocks/ExampleTest.java
@@ -2,45 +2,55 @@ package com.oracle.lal.modernjava.textblocks;
 
 import org.junit.jupiter.api.Test;
 
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Collectors;
-
+/**
+ * Transition between plain old string nad text block
+ * Formatting
+ * Escaping
+ */
 public class ExampleTest {
 
     @Test
-    public void indentation() {
-        var html = """
-                <html>
-                    <head>
-                        <title>TITLE</title>
-                    </head>
-                </html>""";
-        var html2 = """
-                <html>
-                    <head>
-                        <title>TITLE</title>
-                    </head>
-                </html>
-              """;
-        System.out.println(html);
-        System.out.println("========");
-        System.out.println(html2.stripIndent());
-    }
+    public void textBlockExample() {
+        String sqlCase = """
+                SELECT "CustomerName", `City`, 'Country'
+                  FROM      Customers
+                  ORDER BY
+                  
+                         (CASE
+                            WHEN City IS NULL THEN Country
+                            ELSE City
+                         END);
+                """;
+        System.out.println(sqlCase);
+        System.out.println("------");
 
-    @Test
-    public void indentation2() throws Exception {
-        var resource = getClass().getClassLoader().getResource("stripIndent.txt");
-        if (resource != null) {
-            var allLines = Files.readAllLines(Path.of(resource.toURI()));
-            var content = String.join("\n", allLines);
-            System.out.println(content);
-            System.out.println("========");
-            System.out.println(content.stripIndent());
-        } else {
-            System.out.println("Resource not found..");
-        }
+        var html = "<html>\n" +
+                "<head>\n" +
+                "<title>\"Some title\"</title>\n" +
+                "</head>\n" +
+                "</html>";
+        /*
+            <html>\n
+            <head>\n
+            <title>Some title</title>\n
+            </head>\n
+            </html>
+        */
+
+
+        var jsonTextBlock = """
+                   {
+                        window": {
+                            "title": "Main 'app' Window",
+                            "name": "main_window",
+                            "width": 500,
+                            "height": 500
+                        }
+                   }
+                   """;
+
+        System.out.println(html);
+        System.out.println(jsonTextBlock);
+
     }
 }
