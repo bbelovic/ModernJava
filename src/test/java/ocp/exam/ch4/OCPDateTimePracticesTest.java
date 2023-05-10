b1e12f7866e4eb48d75a981948540a7dabd41526
bbelovic <bbelovic@gmail.com>
2023-05-10 08:50:00 +0200

    OCP chapter 4 date & time practise

--- src/test/java/ocp/exam/ch4/OCPDateTimePracticesTest.java
+++ src/test/java/ocp/exam/ch4/OCPDateTimePracticesTest.java
@@ -17,4 +17,24 @@ class OCPDateTimePracticesTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testWinterDaylightSavingTime() {
+        var date = LocalDate.of(2023, Month.NOVEMBER, 5);
+        var time = LocalTime.of(1 ,30);
+        var dateTime = ZonedDateTime.of(date, time, ZoneId.of("US/Eastern"));
+        assertEquals("-04:00", dateTime.getOffset().toString());
+
+        var first = dateTime.plus(Duration.ofHours(1));
+        assertEquals("-05:00", first.getOffset().toString());
+        int hour = first.getHour();
+        int minute = first.getMinute();
+        assertEquals(1, hour);
+        assertEquals(30, minute);
+        var second = first.plus(Duration.ofHours(1));
+        assertEquals("-05:00", second.getOffset().toString());
+        assertEquals(2, second.getHour());
+        assertEquals(30, second.getMinute());
+
+
+    }
 }
